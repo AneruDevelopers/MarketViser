b57b4dfd8c9fc74d900602001c96ab27bd44c2b8
Pedro Todorovski <pedroc.todorovskibr@gmail.com>
2019-04-26 12:52:53 -0300

    Somente teste do sistema

--- TCCSystem/index.php
+++ TCCSystem/index.php
@@ -1,15 +1,12 @@
 <?php
 	$REQUEST_URI = filter_input(INPUT_SERVER, 'REQUEST_URI');
-
 	$INITE = strpos($REQUEST_URI, '?');
 	if($INITE):
 		$REQUEST_URI = substr($REQUEST_URI, 0, $INITE);
 	endif;
-
 	$REQUEST_URI_PASTA = substr($REQUEST_URI, 1);
 	$URL = explode('/', $REQUEST_URI_PASTA);
 	$URL[3] = ($URL[3] != '' ? $URL[3] : 'home');
-
 	if(file_exists('__system__/' . $URL[3] . '.php')):
 		require '__system__/' . $URL[3] . '.php';
 	elseif(is_dir('__system__/' . $URL[3])):
