77edad7b54ccd3af2a2512ae96f5b875384dddd2
nickcarva <carvalhonick2002@gmail.com>
2019-05-01 20:00:08 -0300

    Erro corrigido

--- TCCSystem/__system__/js/listProduto.js
+++ TCCSystem/__system__/js/listProduto.js
@@ -1,4 +1,5 @@
 $(function() {
+    var url = "http://localhost/BackupGit/economize/TCCSystem/__system__/admin_area/imagens_produtos/";
     $.ajax({
         dataType: 'json',
         url: 'functions/listProduto',
@@ -8,7 +9,7 @@ $(function() {
                 produtos[i] = `
                     <div class="divProdCarousel">
                         <a class="linksProdCarousel" href="#">
-                            <img class="divProdImg" src="` + response[i].produto_img + `">
+                            <img class="divProdImg" src="` + url + response[i].produto_img + `">
                             <h4 class="divProdTitle">` + response[i].produto_nome + `</h4>
                             <p class="divProdPrice">R$` + response[i].produto_preco + `</p>
                         </a>
