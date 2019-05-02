20311ea61fea8477aec098717ae87f64866fcc80
nickcarva <carvalhonick2002@gmail.com>
2019-05-01 21:38:02 -0300

    Erro corrigido

--- TCCSystem/__system__/js/listDepartamento.js
+++ TCCSystem/__system__/js/listDepartamento.js
@@ -21,7 +21,15 @@ $(function() {
         url: base + 'functions/listDepartamentos',
         success: function(json) {
             var departs = [];
+            var departsMobile = [];
             for (var i = 0; json.length > i; i++) {
+                departsMobile[i] = `
+                    <div class="celulaMenuCarouselMobile">
+                        <a class="linkBtnMenu" href="` + base + removeAcento(json[i].depart_nome) +`">
+                            <i class="` + json[i].depart_icon + `"></i><h5 class="linkMenuCarouselMobile">` + json[i].depart_nome + `</h5>
+                        </a>
+                    </div>
+                `;
                 departs[i] = `
                     <div class="celulaMenuCarousel">
                         <a class="linkBtnMenu" href="` + base + removeAcento(json[i].depart_nome) +`">
@@ -32,6 +40,7 @@ $(function() {
             }
             for (var i = 0; departs.length > i; i++) {
                 $('.departamentos').append(departs[i]);
+                $('.prodsMobile').append(departsMobile[i]);
             }
             $('body').append('<script src="' + base + '__system__/js/main.js"></script>\
             <script src="' + base + '__system__/js/login.js"></script>');
--- TCCSystem/__system__/teste_session.php
+++ TCCSystem/__system__/teste_session.php
@@ -1,12 +1,49 @@
-<?php
-    session_start();
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+    <title>Document</title>
+    <script>
+        var x = document.getElementById("objeto");
 
-    foreach($_SESSION["tel_num"] as $k => $v) {
-        $key = $k + 1;
-        echo "<b>" . $key . "º Telefone:</b> " . $v . " - <b>Tipo:</b> " . $_SESSION["tipo_tel"][$k] . "<br/>";
-    }
+        function obterLocalizacao() {
+            if(navigator.geoLocation) {
+                navigator.geoLocation.getCurrentPosition(exibirPosicao);
+            } else {
+                x.innerHTML = "Não foi possível realizar este procedimento. A versão atual do navegador impediu o funcionamento.";
+            }
+        }
 
-    foreach($_SESSION["inf_usu"] as $k => $v) {
-        echo "<b>" . $k . ":</b> " . $v . "<br/>";
-    }
-?>
+        function exibirPosicao(position) {
+            x.innerHTML = "Latitude: " + position.coords.latitude + "<br/>Longitude: " + position.coords.longitude;
+        }
+
+        function exibirPosicaoMapa(position) {
+            var latitudeLongitudeFormatado = position.coords.latitude + "," + position.coords.longitude;
+
+            var img_url = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitudeLongitudeFormatado + "&zoom=14&size=400x300&sensor=false&key=YOUR_:KEY";
+
+            document.getElementById("painelDoMapa").innerHTML = "<img src='" + img_url + "'/>";
+        }
+        obterLocalizacao();
+    </script>
+</head>
+<body>
+    <?php
+        require_once 'functions/connection/conn.php';
+
+        if(isset($_SESSION['inf_usu'])) {
+            foreach($_SESSION["tel_num"] as $k => $v) {
+                $key = $k + 1;
+                echo "<b>" . $key . "º Telefone:</b> " . $v . " - <b>Tipo:</b> " . $_SESSION["tipo_tel"][$k] . "<br/>";
+            }
+    
+            foreach($_SESSION["inf_usu"] as $k => $v) {
+                echo "<b>" . $k . ":</b> " . $v . "<br/>";
+            }
+        }
+    ?>
+</body>
+</html>
