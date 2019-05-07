eb4cda008ed5f1562859b53ec9d359419ffa4858
Pedro Todorovski <pedroc.todorovskibr@gmail.com>
2019-05-06 22:35:12 -0300

    Processo

--- TCCSystem/__system__/js/listProduto.js
+++ TCCSystem/__system__/js/listProduto.js
@@ -10,11 +10,7 @@ $(function() {
                     <div class="divProdCarousel">
                         <i class="far fa-heart"></i>
                         <a class="linksProdCarousel" href="#">
-<<<<<<< HEAD
                             <img class="divProdImg" src="__system__/admin_area/imagens_produtos/feijao-preto-urbano-alimentos.png">
-=======
-                            <img class="divProdImg" src="` + url + response[i].produto_img + `">
->>>>>>> ad36057a14430fe42c9dcb53d3c87f80fd00a2b9
                             <h4 class="divProdTitle">` + response[i].produto_nome + `</h4>
                             <p class="divProdPrice">R$` + response[i].produto_preco + `</p>
                             <button class="btnBuy" href="#">COMPRAR</button>
