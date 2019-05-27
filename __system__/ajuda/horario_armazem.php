fd4bbb2778b4d9c167c650c40a1b1f8c070ede18
Pedro Todorovski <pedroc.todorovskibr@gmail.com>
2019-05-27 20:40:42 -0300

    Teste

--- __system__/ajuda/horario_armazem.php
+++ __system__/ajuda/horario_armazem.php
@@ -95,7 +95,6 @@
                                 ';
                             }
                         }
-
                         echo '
                             </table>
                         </div>
