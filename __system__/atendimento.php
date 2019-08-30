018f413ec02744571c7d5f195995232624c62071
Pedro Todorovski <pedroc.todorovskibr@gmail.com>
2019-08-30 19:10:41 -0300

    atualizando

---
+++ __system__/atendimento.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <title>e.conomize | Busca de Produtos</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <link rel="icon" href="<?= base_url(); ?>img/e_icon.png"/>
+    <link rel="stylesheet" type="text/css" media="screen" href="<?= base_url(); ?>/style/css/main.css"/>
+    <link href="<?= base_url(); ?>style/libraries/fontawesome-free-5.8.0-web/css/all.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="<?= base_url(); ?>style/libraries/OwlCarousel2-2.3.4/dist/assets/owl.carousel.min.css" type="text/css"/>
+    <link rel="stylesheet" href="<?= base_url(); ?>style/libraries/OwlCarousel2-2.3.4/dist/assets/owl.theme.default.css" type="text/css"/>
+    <link rel="stylesheet" type="text/css" href="<?= base_url(); ?>style/fonts/Icons/icons_pack/font/flaticon.css"/>
+</head>
+<body>
+    <div class="l-wrapper_FiltroPesq">
+
+        <div class="l-topNav" id="topNav">
+        <?php
+            include('functions/includes/topNav.php');
+        ?>    
+        </div>
+        <nav class="l-headerNav" id="headerNav">
+        <?php
+            include('functions/includes/header.php');
+        ?>
+        </nav>
+        <div class="myModalArmazem" id="myModalArmazem">
+            <div class="modalArmazemContent">
+                <div class="modalArmTopContent">
+                    <div class="meuArmazem">
+                        
+                    </div>
+                    <span class="closeModalArmazem">&times;</span>
+                </div>
+                <div class="modalArmBottomContent">
+                    <div class="Armazens">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="l-main">
+            <h2 class="tituloOfertas"><i class="fas fa-headset"></i> CENTRAL DE ATENDIMENTO</h2>
+            <div class="form_atd divFormContactAttendance">
+            <h3 class="titleDivContactPage">ENVIE UM FEEDBACK</h3>
+                <form id="form-atd" >
+                    <div class="input-form">
+                        <label>NOME:</label><br>
+                        <input type="text" name="name_usu" >
+                    </div>
+                    <div class="input-form">
+                        <label>EMAIL:</label><br>
+                        <input type="text" name="email_usu" >
+                    </div>
+                    <div class="input-form">
+                        <label>CONTEÚDO DA MENSAGEM:</label><br>
+                        <select name="opt">
+                            <optgroup label="Selecione 1:">
+                            <option value="compra">Compra</option>
+                            <option value="carrinho de compra">Carrinho de Compra</option> 
+                            <option value="armazem">Armázens</option> 
+                            <option value="entrega">Entrega</option>
+                            <option value="cidade indisponivel">Cidade Indisponivel</option>
+                            <option value="cadastro">Cadastro</option>
+                            <option value="login">Login</option>     
+                            </optgroup>
+                        
+                        </select>
+                    </div>
+                    <div class="input-form">
+                        <label>MENSAGEM:</label><br>
+                        <textarea placeholder="mensagem" name="txt_usu"></textarea> 
+                    </div>
+                    <div class="input-form">
+                        <button class="btnDivFormContactAttendance" type="submit">ENVIAR</button> 
+                    </div>
+                </form>
+            </div>
+            <div class="divFormContactInformation">
+                <h3 class="titleDivContactPage">INFORMAÇÕES DE CONTATO</h3>
+                <div class="textDivFormContactInformation">
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil sint, aut hic molestias fugiat consectetur, totam corrupti repudiandae incidunt veniam, dicta minus eligendi architecto itaque dolore a numquam porro. Quo!</p>
+                </div>
+                <div class="infoDivFormContactInformation">
+                    <p class="paragLineContactPage"><i class="fas fa-mobile-alt"></i> &nbsp;(14) 99999-9999</p>
+                    <p class="paragLineContactPage"><i class="far fa-envelope"></i> &nbsp;economize.contato@gmail.com</p>
+                    <p>
+                        <a class="linkSocialMediaContactPage" href="#"><i class="fab fa-facebook-square"></i></a> 
+                        &nbsp; 
+                        <a class="linkSocialMediaContactPage" href="#"><i class="fab fa-twitter-square"></i></a>
+                        &nbsp;
+                        <a class="linkSocialMediaContactPage" href="#"><i class="fab fa-instagram"></i></a>
+                        &nbsp;
+                        <a class="linkSocialMediaContactPage" href="#"><i class="fab fa-youtube-square"></i></a>
+                        &nbsp;
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div id="myModal" class="modal">
+            <div class="modal-content">
+                <div class="modalLeftContent">
+                    <form id="form-login">
+                        <!-- <i class="far fa-check-circle"></i> -->
+                        <h4 class="titleModalLogin">LOG IN</h4>
+                        <div class="outsideSecInputCad">
+                            <div class="field -md">
+                                <input type="text" name="usu_email_login" id="usu_email_login" class="placeholder-shown" placeholder="Some placeholder"/>
+                                <label class="labelFieldCad"><strong><i class="fas fa-envelope"></i> EMAIL</strong></label>
+                            </div>
+                            <div class="help-block"></div><br/>
+                        </div>
+                        <div class="outsideSecInputCad">
+                            <div class="field -md">
+                                <input type="password" name="usu_senha_login" id="usu_senha_login" class="placeholder-shown" placeholder="Some placeholder"/>
+                                <label class="labelFieldCad"><strong><i class="fas fa-unlock"></i> SENHA</strong></label>
+                            </div>
+                            <div class="help-block"></div><br/>
+                        </div>
+                        <button class="btnSend" type="submit" id="btn-login" value="Entrar">ENTRAR</button>
+                        <div class="help-block-login"></div>
+                    </form>
+                </div>
+                <div class="modalRightContent">
+                    <span class="close">&times;</span>
+                    <p class="textModal">Olá, amigo!</p>
+                    <p class="textModalBottom">Entre com seus detalhes pessoais e comece sua jornada conosco</p>
+                    <div class="divLinkCad">
+                        <a class="linkCadModal" href="<?= base_url_php(); ?>usuario/cadastro">Cadastre-se já</a>
+                    </div>    
+                </div>
+            </div>
+        </div>
+        <!-- -------------------- -->
+
+        <div class="l-footer" id="footer">
+        <?php
+            include('functions/includes/footer.php');
+        ?>
+        </div>
+        <div class="l-footerBottom" id="footerBottom">
+        <?php
+            include('functions/includes/bottomFooter.html');
+        ?>
+        </div>
+    </div>
+
+    <script src="<?= base_url(); ?>js/JQuery/jquery-3.3.1.min.js"></script>
+    <script src="<?= base_url(); ?>js/JQuery/jquery-mask.js"></script>
+    <script src="<?= base_url(); ?>js/mask.js"></script>
+    <script src="<?= base_url(); ?>style/libraries/sweetalert2.all.min.js"></script>
+    <script src="<?= base_url(); ?>style/libraries/OwlCarousel2-2.3.4/dist/owl.carousel.js"></script>
+    <script src="<?= base_url(); ?>js/util.js"></script>
+    <script src="<?= base_url(); ?>js/verificaLogin.js"></script>
+    <script src="<?= base_url(); ?>js/btnFavorito.js"></script>
+    <script src="<?= base_url(); ?>js/attCarrinho.js"></script>
+    <script src="<?= base_url(); ?>js/listProdutoPromocao.js"></script>
+    <script src="<?= base_url(); ?>js/listDepartamento.js"></script>
+    <script src="<?= base_url(); ?>js/listArmazem.js"></script>
+    <script src="<?= base_url(); ?>js/envatd.js"></script>
+    <?php
+        if(isset($_SESSION['msg_cad'])):?>
+            <script>
+                Swal.fire({
+                    title: "e.conomize informa:",
+                    text: "<?= $_SESSION['msg_cad']['text']; ?>",
+                    type: "error",
+                    showCancelButton: false,
+                    confirmButtonColor: "#A94442",
+                    confirmButtonText: "Ok"
+                });
+            </script>
+            <?php
+            unset($_SESSION['msg_cad']);
+        endif;
+    ?>
+</body>
+</html>
---
+++ __system__/functions/pagseguro/vendor/pagseguro/pagseguro-php-sdk/public/Checkout/modal_compra.php
@@ -0,0 +1,199 @@
+<?php
+require_once '__system__/functions/connection/conn.php';
+
+function getProductsByIds($ids) {
+    global $conn;
+    $sel = $conn->prepare("SELECT * FROM produto AS p JOIN dados_armazem AS d ON p.produto_id=d.produto_id WHERE d.armazem_id={$_SESSION['arm_id']} AND p.produto_id IN (".$ids.")");
+    $sel->execute();
+    if($sel->rowCount() > 0) {
+        $result = $sel->fetchAll();
+        foreach($result as $v) {
+            $dados[] = $v;
+        }
+    }
+    return $dados;
+}
+
+function getContentCart() {
+    global $conn;
+    $results = array();
+    
+    if(isset($_SESSION['carrinho'])) {
+        $cart = $_SESSION['carrinho'];
+        $products =  getProductsByIds(implode(',', array_keys($cart)));
+
+        foreach($products as $k => $product) {
+            if($product['produto_desconto_porcent'] != "") {
+                $product["produto_desconto"] = $product["produto_preco"]*($product["produto_desconto_porcent"]/100);
+                $product["produto_desconto"] = number_format($product["produto_desconto"], 2, '.', '');
+                $product["produto_desconto"] = $product["produto_preco"]-$product["produto_desconto"];
+                $results[$k] = $product;
+            } else {
+                $results[$k] = $product;
+            }
+        }
+    }
+    
+    return $results;
+}
+
+$tel = $conn->prepare("SELECT * FROM telefone AS t JOIN tipo_tel AS tt ON t.tpu_tel=tt.tpu_tel_id WHERE t.usu_id=:id");
+$tel->bindValue(":id", "{$_SESSION['inf_usu']['usu_id']}");
+$tel->execute();
+$res = $tel->fetchAll();
+
+/**
+ * 2007-2016 [PagSeguro Internet Ltda.]
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author    PagSeguro Internet Ltda.
+ * @copyright 2007-2016 PagSeguro Internet Ltda.
+ * @license   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ */
+
+require_once "__system__/functions/pagseguro/vendor/autoload.php";
+
+\PagSeguro\Library::initialize();
+\PagSeguro\Library::cmsVersion()->setName("Nome")->setRelease("1.0.0");
+\PagSeguro\Library::moduleVersion()->setName("Nome")->setRelease("1.0.0");
+
+?>
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <!-- 
+        EMAIL COMPRADOR: c42358207331366747669@sandbox.pagseguro.com.br
+        SENHA: 4303c40373589875
+    -->
+        <?php if (\PagSeguro\Configuration\Configure::getEnvironment()->getEnvironment() == "sandbox") : ?>
+            <!--Para integração em ambiente de testes no Sandbox use este link-->
+            <script
+                    type="text/javascript"
+                    src="https://stc.sandbox.pagseguro.uol.com.br/pagseguro/api/v2/checkout/pagseguro.lightbox.js">
+            </script>
+        <?php else : ?>
+            <!--Para integração em ambiente de produção use este link-->
+            <script
+                    type="text/javascript"
+                    src="https://stc.pagseguro.uol.com.br/pagseguro/api/v2/checkout/pagseguro.lightbox.js">
+            </script>
+        <?php endif; ?>
+    </head>
+    </html>
+
+<?php
+
+$payment = new \PagSeguro\Domains\Requests\Payment();
+
+$resultsCarts = getContentCart();
+foreach($resultsCarts as $k => $v) {
+    if($v['produto_desconto_porcent'] != "") {
+        $payment->addItems()->withParameters(
+            $v['produto_id'],
+            $v['produto_nome'],
+            $_SESSION['carrinho'][$v['produto_id']],
+            $v['produto_desconto']
+        );
+    } else {
+        $payment->addItems()->withParameters(
+            $v['produto_id'],
+            $v['produto_nome'],
+            $_SESSION['carrinho'][$v['produto_id']],
+            $v['produto_preco']
+        );
+    }
+}
+
+if(isset($_SESSION['cupom_compra'])) {
+    $totCupom = $_SESSION['totCompraCupom']*($_SESSION['cupom_compra']['cupom_desconto_porcent']/100);
+    $totCupom = number_format($totCupom,2,'.','');
+    $payment->setExtraAmount('-' . $totCupom);
+}
+
+$payment->setCurrency("BRL");
+$payment->setReference("ECONOMIZE0101");
+
+// Set your customer information.
+$payment->setSender()->setName($_SESSION['inf_usu']['usu_nome']. ' ' . $_SESSION['inf_usu']['usu_sobrenome']);
+$payment->setSender()->setEmail($_SESSION['inf_usu']['usu_email']);
+foreach($res as $v) {
+    $v['ddd'] = substr($v['tel_num'],1,2);
+    $v['num'] = substr($v['tel_num'],-10);
+    if(strpos($v['num']," ")) {
+        $v['num'] = str_replace($v['num']," ","");
+    }
+    $v['num'] = str_replace($v['num'], "-", "");
+
+    $payment->setSender()->setPhone()->withParameters(
+        $v['ddd'],
+        $v['num']
+    );
+}
+
+$cpf = str_replace($_SESSION['inf_usu']['usu_cpf'],"-","");
+$cpf = str_replace($_SESSION['inf_usu']['usu_cpf'],".","");
+$payment->setSender()->setDocument()->withParameters(
+    $cpf,
+    '07650258000182'
+);
+
+$payment->setShipping()->setAddress()->withParameters(
+    $_SESSION['end_agend'][1],
+    $_SESSION['end_agend'][2],
+    $_SESSION['end_agend'][4],
+    $_SESSION['end_agend'][0],
+    $_SESSION['end_agend'][5],
+    $_SESSION['end_agend'][6],
+    'BRA',
+    $_SESSION['end_agend'][3]
+);
+$payment->setShipping()->setCost()->withParameters(00.00);
+$payment->setShipping()->setType()->withParameters(\PagSeguro\Enum\Shipping\Type::SEDEX);
+
+//Add metadata items
+$payment->addMetadata()->withParameters('PASSENGER_CPF', '15986210093');
+$payment->addMetadata()->withParameters('GAME_NAME', 'DOTA');
+$payment->addMetadata()->withParameters('PASSENGER_PASSPORT', '23456', 1);
+
+//Add items by parameter
+// $payment->addParameter()->withParameters('itemId', '0003')->index(3);
+// $payment->addParameter()->withParameters('itemDescription', 'Notebook Rosa')->index(3);
+// $payment->addParameter()->withParameters('itemQuantity', '1')->index(3);
+// $payment->addParameter()->withParameters('itemAmount', '201.40')->index(3);
+
+//Add items by parameter using an array
+//$payment->addParameter()->withArray(['notificationURL', 'http://www.lojamodelo.com.br/nofitication']);
+
+
+$payment->setRedirectUrl(base_url_php() . "compra/extrato");
+$payment->setNotificationUrl(base_url_php() . "admin_area/notificacao");
+
+try {
+    $onlyCheckoutCode = true;
+    $result = $payment->register(
+        \PagSeguro\Configuration\Configure::getAccountCredentials(),
+        $onlyCheckoutCode
+    );
+
+    echo "
+        <img src='../__system__\img\E_zinho for Phone.png' class='imgLogoPagPage' alt='Equipe e.conomize'><br>
+        <h2 class='warningMsgPag'>Criando requisi&ccedil;&atilde;o de pagamento. Aguarde...</h2>"
+        . "<p class='warningMsgPag'>C&oacute;digo da transa&ccedil;&atilde;o: <strong>" . $result->getCode() . "</strong></p>"
+        . "<script>PagSeguroLightbox('" . $result->getCode() . "');</script>";
+} catch (Exception $e) {
+    die($e->getMessage());
+}
